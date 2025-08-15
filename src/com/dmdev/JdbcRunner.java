11b0bb1f3d0c7a37498bf92f617227125cde1cdb
Roman <astek14@mail.ru>
2025-08-15 11:45:35 +0300

    Statement DML operations

--- src/com/dmdev/JdbcRunner.java
+++ src/com/dmdev/JdbcRunner.java
@@ -10,17 +10,19 @@ public class JdbcRunner {
     public static void main(String[] args) throws SQLException {
         Class<Driver> driverClass = Driver.class;
         String sql = """
-                CREATE TABLE IF NOT EXISTS info(
-                    id SERIAL PRIMARY KEY,
-                    data TEXT NOT NULL
-                );
+                 UPDATE info
+                 SET data = 'TestTest'
+                 WHERE id = 5
+                 RETURNING *
                 """;
         try (var connection = ConnectionManager.open();
              var statement = connection.createStatement()) {
             System.out.println(connection.getSchema());
             System.out.println(connection.getTransactionIsolation());
+//            var executedResult = statement.executeUpdate(sql);
             var executedResult = statement.execute(sql);
             System.out.println(executedResult);
+            System.out.println(statement.getUpdateCount());
         }
     }
 }
