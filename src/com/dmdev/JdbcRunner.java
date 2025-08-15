9880dc1233051c3c6a20d84401defca4bae890b5
Roman <astek14@mail.ru>
2025-08-15 12:12:27 +0300

    ResultSet operation Select

--- src/com/dmdev/JdbcRunner.java
+++ src/com/dmdev/JdbcRunner.java
@@ -3,6 +3,7 @@ package com.dmdev;
 import com.dmdev.util.ConnectionManager;
 import org.postgresql.Driver;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class JdbcRunner {
@@ -10,19 +11,23 @@ public class JdbcRunner {
     public static void main(String[] args) throws SQLException {
         Class<Driver> driverClass = Driver.class;
         String sql = """
-                 UPDATE info
-                 SET data = 'TestTest'
-                 WHERE id = 5
-                 RETURNING *
+                 SELECT *
+                 FROM ticket
                 """;
         try (var connection = ConnectionManager.open();
-             var statement = connection.createStatement()) {
+             var statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);) {
             System.out.println(connection.getSchema());
             System.out.println(connection.getTransactionIsolation());
-//            var executedResult = statement.executeUpdate(sql);
-            var executedResult = statement.execute(sql);
-            System.out.println(executedResult);
-            System.out.println(statement.getUpdateCount());
+
+            var executedResult = statement.executeQuery(sql);
+            while (executedResult.next()) {
+                System.out.println(executedResult.getLong("id"));
+                System.out.println(executedResult.getString("passenger_no"));
+                System.out.println(executedResult.getBigDecimal("cost"));
+//                executedResult.updateLong("id", 1000); лучше такого не делать, нужно включать этот режим (ResultSet.CONCUR_UPDATABLE)
+//                ResultSet.TYPE_SCROLL_INSENSITIVE можем прокручивать вперед/назад, будем видеть измененные строки во время итерированная или нет
+                System.out.println("-------");
+            }
         }
     }
 }
