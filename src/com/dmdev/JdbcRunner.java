4f8fc86d212da8c0516040e30d9ec214961bdcfe
Roman <astek14@mail.ru>
2025-08-15 10:30:51 +0300

    connection to DB

--- src/com/dmdev/JdbcRunner.java
+++ src/com/dmdev/JdbcRunner.java
@@ -1,10 +1,16 @@
 package com.dmdev;
 
+import com.dmdev.util.ConnectionManager;
 import org.postgresql.Driver;
+
+import java.sql.SQLException;
 
 public class JdbcRunner {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         Class<Driver> driverClass = Driver.class;
+        try (var connection = ConnectionManager.open()) {
+            System.out.println(connection.getTransactionIsolation());
+        }
     }
 }
