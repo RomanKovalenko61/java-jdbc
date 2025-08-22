0a8a80974c38cd5e2ebc4ba1f48b9291f41b99da
Roman <astek14@mail.ru>
2025-08-22 11:34:11 +0300

    DAO Entity Mapping

---
+++ src/com/dmdev/entity/Ticket.java
@@ -0,0 +1,82 @@
+package com.dmdev.entity;
+
+import java.math.BigDecimal;
+
+public class Ticket {
+
+    private Long id;
+    private String passengerNo;
+    private String passengerName;
+    private Long flightId;
+    private String seatNo;
+    private BigDecimal coat;
+
+    public Ticket(Long id, String passengerNo, String passengerName, Long flightId, String seatNo, BigDecimal coat) {
+        this.id = id;
+        this.passengerNo = passengerNo;
+        this.passengerName = passengerName;
+        this.flightId = flightId;
+        this.seatNo = seatNo;
+        this.coat = coat;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPassengerNo() {
+        return passengerNo;
+    }
+
+    public void setPassengerNo(String passengerNo) {
+        this.passengerNo = passengerNo;
+    }
+
+    public String getPassengerName() {
+        return passengerName;
+    }
+
+    public void setPassengerName(String passengerName) {
+        this.passengerName = passengerName;
+    }
+
+    public Long getFlightId() {
+        return flightId;
+    }
+
+    public void setFlightId(Long flightId) {
+        this.flightId = flightId;
+    }
+
+    public String getSeatNo() {
+        return seatNo;
+    }
+
+    public void setSeatNo(String seatNo) {
+        this.seatNo = seatNo;
+    }
+
+    public BigDecimal getCoat() {
+        return coat;
+    }
+
+    public void setCoat(BigDecimal coat) {
+        this.coat = coat;
+    }
+
+    @Override
+    public String toString() {
+        return "Ticket{" +
+                "id=" + id +
+                ", passengerNo='" + passengerNo + '\'' +
+                ", passengerName='" + passengerName + '\'' +
+                ", flightId=" + flightId +
+                ", seatNo='" + seatNo + '\'' +
+                ", coat=" + coat +
+                '}';
+    }
+}
